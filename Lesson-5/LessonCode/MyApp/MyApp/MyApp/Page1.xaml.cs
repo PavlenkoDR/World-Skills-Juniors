d5296ee7400ac08db23d46f7878bf81a9903f075
Павленко Дмитрий <pavlenkodima1996@mail.ru>
2018-12-14 00:40:18 +1000

    Lesson5

---
+++ Lesson-5/LessonCode/MyApp/MyApp/MyApp/Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+        async void Load( int ms )
+        {
+            for (int i = 0; i < ms / 4; i++)
+            {
+                label1.Opacity = i * 4.0 / ms;
+                await Task.Delay(1);
+            }
+            await Task.Delay(ms / 2);
+            for (int i = 0; i < ms / 4; i++)
+            {
+                label1.Opacity = ( ms / 4 - i ) * 4.0 / ms;
+                await Task.Delay(1);
+            }
+            Application.Current.MainPage = new MainPage();
+            
+            //await Navigation.PushModalAsync(new NavigationPage(new MainPage()));
+        }
+
+		public Page1 ()
+		{
+			InitializeComponent ();
+            Load(500);
+        }
+	}
+}
---
+++ Lesson-5/LessonCode/MyApp/MyApp/MyApp/View3.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class View3 : ContentPage
+	{
+		public View3 ()
+		{
+       
+		InitializeComponent ();
+		}
+
+        async void auth (string login, string password)
+        {
+            if(login == "admin" && password == "admin")
+            {
+                autt.Text = "It's alright!!!";
+            }
+            else
+            {
+                autt.Text = "LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOSER";
+                await Task.Delay(5000);
+                MainPage rootPage = Application.Current.MainPage as MainPage;
+                rootPage.Detail = new NavigationPage(new View1() );
+                await Navigation.PushModalAsync(new NavigationPage(new View1()));
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            auth(l.Text, p.Text);
+        }
+    }
+}
