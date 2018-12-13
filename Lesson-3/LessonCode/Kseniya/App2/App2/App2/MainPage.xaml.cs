0d0eb63655c569230323dd068bf738ebd1845198
Павленко Дмитрий <pavlenkodima1996@mail.ru>
2018-12-13 10:46:41 +1000

    Kseniya code

---
+++ Lesson-3/LessonCode/Kseniya/App2/App2/App2/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Label1.Text = "Nuu";
+            Box1.BackgroundColor = Color.Pink;
+        }
+    }
+}
