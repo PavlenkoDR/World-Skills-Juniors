31364a1b7d7b1a324cd54893d661b82cb43aa84e
Павленко Дмитрий <pavlenkodima1996@mail.ru>
2018-12-15 17:55:29 +1000

    Update

---
+++ Lesson-5/LessonCode/ClassCode/MyApp/MyApp/MyApp/View3.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class View3 : ContentPage
+	{
+		public View3 ()
+		{
+       
+		InitializeComponent ();
+		}
+
+        void auth (string login, string password)
+        {
+            if(login == "admin" && password == "admin")
+            {
+                autt.Text = "It's alright!!!";
+            }
+            else
+            {
+                autt.Text = "LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOSER";
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            auth(l.Text ,p.Text);
+        }
+    }
+}
