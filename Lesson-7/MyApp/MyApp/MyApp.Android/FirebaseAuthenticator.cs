168efca752267274bfd002d45e151d4bc520ecab
Павленко Дмитрий <pavlenkodima1996@mail.ru>
2018-12-18 23:35:44 +1000

    Update Lessons

---
+++ Lesson-7/MyApp/MyApp/MyApp.Android/FirebaseAuthenticator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Auth;
+using FirebaseAuthentication;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(MyApp.Droid.FirebaseAuthenticator))]
+namespace MyApp.Droid
+{
+    public class FirebaseAuthenticator : IFirebaseAuthenticator
+    {
+        public async Task<string> LoginWithEmailPassword(string email, string password)
+        {
+            IAuthResult user;
+            try
+            {
+                user = await FirebaseAuth.Instance.
+                             SignInWithEmailAndPasswordAsync(email, password);
+            }
+            catch (Exception)
+            {
+                return "user error";
+            }
+            GetTokenResult token;
+            try
+            {
+                token = await user.User.GetIdTokenAsync(false);
+            }
+            catch (Exception)
+            {
+                return "token error";
+            }
+            return token.Token;
+        }
+
+        public async Task<string> LoginWithEmailAnonymously()
+        {
+            IAuthResult user;
+            try
+            {
+                user = await FirebaseAuth.Instance.
+                             SignInAnonymouslyAsync();
+            }
+            catch (Exception)
+            {
+                return "user error";
+            }
+            GetTokenResult token;
+            try
+            {
+                token = await user.User.GetIdTokenAsync(false);
+            }
+            catch (Exception)
+            {
+                return "token error";
+            }
+            return token.Token;
+        }
+
+        public async Task<string> RegsiterWithEmailPassword(string email, string password)
+        {
+            var user = await FirebaseAuth.Instance.
+                                                CreateUserWithEmailAndPasswordAsync(email, password);
+            var token = await user.User.GetIdTokenAsync(false);
+            return token.Token;
+        }
+    }
+}
---
+++ Lesson-7/MyApp/MyApp/MyApp/Page3.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyApp
+{
+    public interface IAuth
+    {
+       Task<string> LogIn(string L, string P);
+
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page3 : ContentPage
+	{
+        
+		public Page3 ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            label1.Text = await DependencyService.Get<IAuth>().LogIn("gf", "gf");
+        }
+    }
+}
